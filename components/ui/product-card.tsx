f3377052a6f7b505259edddfc17da766eadc0d1f
Khoa Nguyen <henrykhoanguyen@gmail.com>
2024-01-02 23:56:23 -0600

    finished product page working on category page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,16 +6,26 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Images and Actions */}
-      <div className="aspect-square rounded-xl bg-gray-100 relative">
+      <div
+        onClick={handleClick}
+        className="aspect-square rounded-xl bg-gray-100 relative"
+      >
         <Image
           src={data?.images?.[0]?.url}
           fill
--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "**",
-      },
-    ],
+    domains: ["res.cloudinary.com"],
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "res.cloudinary.com",
+  //       pathname: "**",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
