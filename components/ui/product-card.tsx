d43396fdb6a0af6cb96fd82b9ea9f2b310640d76
Khoa Nguyen <henrykhoanguyen@gmail.com>
2024-01-04 21:37:25 -0600

    created preview modal

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
-import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
-import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCard {
   data: Product;
@@ -14,9 +16,16 @@ interface ProductCard {
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const router = useRouter();
-
+  const previewModal = usePreviewModal();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    // stop the main div to execute onClick(handleClick)
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -35,7 +44,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
